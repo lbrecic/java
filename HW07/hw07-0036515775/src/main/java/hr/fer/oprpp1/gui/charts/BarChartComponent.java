ac95bc55dbf9c562ccc84f1e99895620fddfc79f
Luka Brečić <luka.brecic@fer.hr>
2021-01-19 22:04:25 +0100

    Started 3rd task of 7th homework assignment.

---
+++ HW07/hw07-0036515775/src/main/java/hr/fer/oprpp1/gui/charts/BarChartComponent.java
@@ -0,0 +1,16 @@
+package hr.fer.oprpp1.gui.charts;
+
+import javax.swing.JComponent;
+
+public class BarChartComponent extends JComponent {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private BarChart chart;
+	
+	public BarChartComponent(BarChart chart) {
+		this.chart = chart;
+	}
+	
+	
+}
