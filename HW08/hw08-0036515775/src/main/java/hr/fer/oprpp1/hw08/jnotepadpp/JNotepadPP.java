b1c04cd0a43344c34ab1159e8eb5f35bb28cbc2a
Luka Brečić <luka.brecic@fer.hr>
2021-01-25 21:48:32 +0100

    Simple modifications.

--- HW08/hw08-0036515775/src/main/java/hr/fer/oprpp1/hw08/jnotepadpp/JNotepadPP.java
+++ HW08/hw08-0036515775/src/main/java/hr/fer/oprpp1/hw08/jnotepadpp/JNotepadPP.java
@@ -1,9 +1,12 @@
 package hr.fer.oprpp1.hw08.jnotepadpp;
 
 import java.awt.Container;
+import java.awt.LayoutManager;
+import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
+import javax.swing.JToolBar;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
@@ -23,14 +26,16 @@ public class JNotepadPP extends JFrame {
 		setTitle("JNotepad++");
 		setSize(300, 300);
 		setLocation(300, 300);
+		setLayout(new BorderLayout());
 		
 		initGUI();
 	}
 	
 	public void initGUI() {
 		Container cp = getContentPane();
+		JToolBar tool = new JToolBar();
 		model = new DefaultMultipleDocumentModel();
-		cp.add((JTabbedPane) model);
+		cp.add((JTabbedPane) model, BorderLayout.CENTER);
 		
 	}
 	
