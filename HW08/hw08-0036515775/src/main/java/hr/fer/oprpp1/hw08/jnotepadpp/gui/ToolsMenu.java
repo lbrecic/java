041b3a29e1d70c4998407bb6a9349b26e484ec4f
Luka Brečić <luka.brecic@fer.hr>
2021-02-06 23:17:12 +0100

    Closing bug fixed.

--- HW08/hw08-0036515775/src/main/java/hr/fer/oprpp1/hw08/jnotepadpp/gui/ToolsMenu.java
+++ HW08/hw08-0036515775/src/main/java/hr/fer/oprpp1/hw08/jnotepadpp/gui/ToolsMenu.java
@@ -14,56 +14,60 @@ import hr.fer.oprpp1.hw08.jnotepadpp.models.DefaultMultipleDocumentModel;
 public class ToolsMenu extends LJMenu {
 
 	private static final long serialVersionUID = 1L;
-	
-	public ToolsMenu(JNotepadPPActions actions, DefaultMultipleDocumentModel docModel, String key, ILocalizationProvider flp) {
+
+	public ToolsMenu(JNotepadPPActions actions, DefaultMultipleDocumentModel docModel, String key,
+			ILocalizationProvider flp) {
 		super(key, flp);
 		addToolsMenuItems(actions, docModel, flp);
 	}
-	
-	public void addToolsMenuItems(JNotepadPPActions actions, DefaultMultipleDocumentModel docModel, ILocalizationProvider flp) {
+
+	public void addToolsMenuItems(JNotepadPPActions actions, DefaultMultipleDocumentModel docModel,
+			ILocalizationProvider flp) {
 		LJMenu change = new LJMenu("case", flp);
 		change.add(new LJMenuItem(actions.getToUppercaseAction(), "up", flp));
 		change.add(new LJMenuItem(actions.getToLowercaseAction(), "low", flp));
 		change.add(new LJMenuItem(actions.getInvertCaseAction(), "inv", flp));
-		
+
 		add(change);
-		
+
 		LJMenu sort = new LJMenu("sort", flp);
 		sort.add(new LJMenuItem(actions.getAscendingAction(), "asc", flp));
 		sort.add(new LJMenuItem(actions.getDescendingAction(), "desc", flp));
-		
+
 		add(sort);
-		
+
 		LJMenuItem unique = new LJMenuItem(actions.getUniqueAction(), "unique", flp);
 		add(unique);
-		
+
 		change.setEnabled(false);
 		sort.setEnabled(false);
 		unique.setEnabled(false);
-		
+
 		docModel.addChangeListener(new ChangeListener() {
-			
+
 			@Override
 			public void stateChanged(ChangeEvent e) {
-				docModel.getCurrentDocument().getTextComponent().addCaretListener(new CaretListener() {
-					
-					@Override
-					public void caretUpdate(CaretEvent e) {
-						if (docModel.getCurrentDocument().getTextComponent().getSelectedText() != null) {
-							change.setEnabled(true);
-							sort.setEnabled(true);
-							unique.setEnabled(true);
-						} else {
-							change.setEnabled(false);
-							sort.setEnabled(false);
-							unique.setEnabled(false);
+				if (docModel.getCurrentDocument() != null) {
+					docModel.getCurrentDocument().getTextComponent().addCaretListener(new CaretListener() {
+
+						@Override
+						public void caretUpdate(CaretEvent e) {
+							if (docModel.getCurrentDocument().getTextComponent().getSelectedText() != null) {
+								change.setEnabled(true);
+								sort.setEnabled(true);
+								unique.setEnabled(true);
+							} else {
+								change.setEnabled(false);
+								sort.setEnabled(false);
+								unique.setEnabled(false);
+							}
 						}
-					}
-					
-				});
+
+					});
+				}
 			}
-			
+
 		});
 	}
-	
+
 }
